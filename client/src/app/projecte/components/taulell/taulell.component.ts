3700d68cf28f060988862548496c464e87467ca2
jaumee99 <jcasamitjana8@boscdelacoma.cat>
2023-03-14 19:02:02 +0100

    examen

--- client/src/app/projecte/components/taulell/taulell.component.ts
+++ client/src/app/projecte/components/taulell/taulell.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray, transferArrayItem  } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Cell } from '../../models/cell/cell';
+import { Board } from '../../models/board/board';
 
 @Component({
   selector: 'app-taulell',
@@ -8,92 +9,36 @@ import { Cell } from '../../models/cell/cell';
   styleUrls: ['./taulell.component.css']
 })
 export class TaulellComponent implements OnInit {
-  board: Cell[][] = [];
   capturedPieces: string[] = [];
+  private _board: Board;
 
   constructor() {
-    this.initializeBoard();
+    this._board = new Board();
   }
 
   ngOnInit(): void {
-    
+
+  }
+
+  get board(): Board {
+    return this._board;
   }
 
   getCellClass(cell: Cell) {
     return ['cell', cell.color];
   }
 
-  drop(event: CdkDragDrop<any>): void {
-    moveItemInArray(
-      event.container.data,
-      event.previousIndex,
-      event.currentIndex
-    );
+  drop(event: any) {
+    const previousCell: Cell = event.previousContainer.data;
+    const currentCell: Cell = event.container.data;
+    console.log(previousCell);
+    console.log(currentCell);
+    if (event.previousContainer === event.container) {
+      console.log('same container');
+    } else {
+      console.log('different container');
+    }
   }
 
-  onDrop(event: CdkDragDrop<any>): void {
-    const previousCell = event.previousContainer.data;
-    const currentCell = event.container.data;
-  
-    if (previousCell === currentCell) {
-      return;
-    }
-  
-    if (currentCell.piece) {
-      this.capturedPieces.push(previousCell.piece);
-      previousCell.piece = '';
-    }
-  
-    transferArrayItem(
-      event.previousContainer.data,
-      event.container.data,
-      event.previousIndex,
-      event.currentIndex
-    );
-  }
-  
-  
-
-  private initializeBoard() {
-    for (let i = 0; i < 8; i++) {
-      this.board.push([]);
-      for (let j = 0; j < 8; j++) {
-        this.board[i].push({
-          piece: '',
-          color: (i + j) % 2 === 0 ? 'white' : 'black'
-        });
-      }
-    }
-
-    for (let i = 0; i < 8; i++) {
-      this.board[1][i].piece = 'assets/bP.png';
-      this.board[6][i].piece = 'assets/wP.png';
-    }
-
-    this.board[0][0].piece = 'assets/bR.png';
-    this.board[0][7].piece = 'assets/bR.png';
-    this.board[7][0].piece = 'assets/wR.png';
-    this.board[7][7].piece = 'assets/wR.png';
-
-    this.board[0][1].piece = 'assets/bN.png';
-    this.board[0][6].piece = 'assets/bN.png';
-    this.board[7][1].piece = 'assets/wN.png';
-    this.board[7][6].piece = 'assets/wN.png';
-
-    this.board[0][2].piece = 'assets/bB.png';
-    this.board[0][5].piece = 'assets/bB.png';
-    this.board[7][2].piece = 'assets/wB.png';
-    this.board[7][5].piece = 'assets/wB.png';
-
-    this.board[0][3].piece = 'assets/bQ.png';
-    this.board[7][3].piece = 'assets/wQ.png';
-
-    this.board[0][4].piece = 'assets/bK.png';
-    this.board[7][4].piece = 'assets/wK.png';
-  }
-
-  getCell(row: number, col: number): Cell {
-    return this.board[row][col];
-  }
 }
 
